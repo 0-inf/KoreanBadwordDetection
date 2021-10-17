296f91296db1931ea7ab6ee5e24aa520b1eb0ad6
Seol7523 <seolchaehan@gmail.com>
2021-10-17 20:15:19 +0900

    docs 작성

--- WordDetection.py
+++ WordDetection.py
@@ -1,14 +1,19 @@
 import pickle
+from typing import List
 
 korean_one = ['ㄱ','ㄲ','ㄴ','ㄷ','ㄸ','ㄹ','ㅁ','ㅂ','ㅃ','ㅅ','ㅆ','ㅇ','ㅈ','ㅉ','ㅊ','ㅋ','ㅌ','ㅍ','ㅎ'] # 한글 초성
 korean_two = ['ㅏ','ㅐ','ㅑ','ㅒ','ㅓ','ㅔ','ㅕ','ㅖ','ㅗ','ㅘ','ㅙ','ㅚ','ㅛ','ㅜ','ㅝ','ㅞ','ㅟ','ㅠ','ㅡ','ㅢ','ㅣ'] # 한글 중성
 korean_three = ['','ㄱ','ㄲ','ㄳ','ㄴ','ㄵ','ㄶ','ㄷ','ㄹ','ㄺ','ㄻ','ㄼ','ㄽ','ㄾ','ㄿ','ㅀ','ㅁ','ㅂ','ㅄ','ㅅ','ㅆ','ㅇ','ㅈ','ㅊ','ㅋ','ㅌ','ㅍ','ㅎ'] # 한글 종성
 
-def detach_word(word,before):
+def detach_word(word : List[str,int],before : List) -> List:
     """
-    word에 한글이 입력되면 초성,중성,종성으로 쪼개줍니다
-    한글이 아니면 그냥 넘김니다
+    한국어를 초성,중성,종성으로 분해해줍니다.
+
+    :param word: 리스트 타입 [(분해할 한글),(글자의 현재 위치)]로 분해될 한글입니다.
+    :param before: 경우에 따라서 초성'ㅇ'을 제거하기 위한 인자로 리스트를 받습니다.
+    :return: 리스트타입으로 [[(초성),(글자의 분해 전 위치)],[(중성),(글자의 분해 전 위치)],[(종성),(글자의 분해 전 위치)]]를 리턴합니다.
     """
+
     result = []
     askicode = ord(word[0]) - 44032
     if -1 < askicode and askicode < 11173:
@@ -30,29 +35,33 @@ def detach_word(word,before):
         result.append(word)
     return result
 
-def MakeBetter(x:int):
+def MakeBetter(x : int) -> int:
     """
-    글자수가 짧을 수록 더 엄격하게 확률을 적용합니다
+    글자수가 짧을수록 더 엄격하게 확률을 적용하기 위한 가중치 함수입니다.
+
+    :param x: 글자수를 입력받습니다.
+    :return: 가중치를 리턴합니다.
     """
     return 0.1**((x-3)/10)+1
 
 
 class WordDetection():
+    """
+    파이썬 욕설 탐지 모듈입니다
+    """
 
-    def __init__(self):
-
+    def __init__(self) -> None:
         """
-        처리 과정에 필요한 데이터
+        초깃값을 설정합니다
         """
+        
         self.BaseL = {} #Base layer 데이터
         self.SeemL = {} #Seem layer 데이터
         self.KeBoL = {} #KeyBorad layer 데이터
         self.PronL = {} #Pro layer 데이터
 
 
-        """
-        처리후 생기는 데이터
-        """
+        
         self.input = '' # 입력된 문자열
         self.WTD = [] #Token 화 , Detach 모두 완료된 입력 문자열의 리스트
         self.BwNt = [] #Token화가 되지않은 badword의 리스트
@@ -63,25 +72,25 @@ class WordDetection():
 
         
 
-    def LoadData(self , respon = False):
+    def LoadData(self) -> None:
         """
-        각 layer들의 데이터를 로딩해옵니다(WDLD.txt로부터 읽어옵니다)
-        respon이 True일시에는 해당 레이어의 값들을 return 해줍니다.
+        각 layer들의 데이터를 로딩해옵니다 (WDLD.txt로부터 읽어옵니다)
+
+        :return: 아무것도 리턴하지 않습니다.
         """
         with open('WDLD.txt', 'rb') as f:
             self.BaseL = pickle.load(f)
             self.SeemL = pickle.load(f)
             self.KeBoL = pickle.load(f)
             self.PronL = pickle.load(f)
-        if respon is True:
-            return [self.BaseL , self.SeemL , self.KeBoL , self.PronL]
-        else:
-            return None
+        return None
     
-    def LoadBadWordData(self,file="Badwords.txt"):
+    def LoadBadWordData(self,file : str ="Badwords.txt") -> None:
         """
         욕설 데이터를 불러오고 자동으로 저장합니다.
-        file은 욕설 데이터를 불러올 경로입니다.
+        
+        :param file: 욕설 리스트 파일의 주소입니다.
+        :return: 아무것도 리턴하지 않습니다.
         """
         f=open(file,'r',encoding="utf-8")
         while True:
@@ -90,13 +99,16 @@ class WordDetection():
             self.AddBW(line[0:-1])
         f.close()
         self.TokenBW()
+        return None
 
 
 
-    def AddBW(self , badword:str , respon = False):
+    def AddBW(self , badword : str) -> None:
         """
-        BadWord를 입력받습니다.
-        입력된 값을 self.BwNt에 추가합니다
+        BadWord를 입력받아 self.BwNt 또는 self.NewBwNT에 저장합니다.
+
+        :param badword: 추가할 욕설입니다.
+        :return: 아무것도 리턴하지 않습니다.
         """
         if badword in self.BwNt:
             return None
@@ -111,14 +123,13 @@ class WordDetection():
         else:
             if badword not in self.BwNt:
                 self.BwNt.append(badword)
-            if respon is True:
-                return badword
-            else:
-                return None
+        return None
 
-    def TokenBW(self , respon = False):
+    def TokenBW(self) -> None:
         """
-        badwords를 토큰화 합니다.
+        self.BwNt와 self.NewBwNT에 저장되어있는 욕설들을 톤큰화합니다.
+
+        :return: 아무것도 리턴하지 않습니다.
         """
         result = []
         for i in self.BwNt:
@@ -139,22 +150,17 @@ class WordDetection():
                 ilist.append([self.BaseL[i[j]],j])
             result.append(ilist)
         self.NewBwT = result
+        return None
 
-        if respon is True:
-            return result
-        else:
-            return None
-
-    def W2NR(self):
+    def W2NR(self) -> None:
         """
-        self.input 을 자동으로 처리하여 self.WTD로 입력합니다
+        self.input에 저장된 문자열을 자동으로 처리하여 self.WTD에 저장합니다
+
+        :return: 아무것도 리턴하지 않습니다.
         """
         PassList = [' ','이']
         result = []
         word = self.input
-        """
-        단어 쪼개고 중복 제거
-        """
         for i in range(len(word)):
             if word[i] not in PassList:
                 if i == len(word)-1:
@@ -166,9 +172,6 @@ class WordDetection():
                         result.append([self.input[i],i])
             else:
                 pass
-        """
-        초성 , 중성 ,종성 분리하기
-        """
         result1 = []
         new_layer=[]  #초성의 번호
         for i in range(0,len(result)):
@@ -218,16 +221,17 @@ class WordDetection():
                     new_re[2].append((self.BaseL[i[0]],i[1]))
             else:
                 pass
-            
-                
         result = result1
         self.WTD = [result,new_re]
         return None
 
-    def word_comparing(self , compare_word , compare_badword):
+    def word_comparing(self , compare_word : List, compare_badword : List) -> int:
         """
-        compare_word에 입력된 값을 compare_badword와 비교합니다.
-        compare_word와 compare_badword에 입력되는 값은 길이가 같아야하고 토큰화된 후 이여야 합니다
+        compare_word에 입력된 값과 compare_badword의 유사도를 비교합니다.
+
+        :param compare_word: 토큰화가 된 확인할 문장의 일부분입니다.
+        :param compare_badword: 토큰화가 된 비교할 욕설입니다.
+        :return: 두 입력값의 유사도를 0과 1사이로 리턴합니다.
         """
         a = 0
         if len(compare_word) != len(compare_badword):
@@ -249,9 +253,15 @@ class WordDetection():
         return same ** better
         
 
-    def lime_compare(self, badwords , compare_word,cut_line,New):
+    def lime_compare(self, badwords : List = self.BwT, compare_word : List = self.WTD[0], cut_line : int = 0.9 , New : bool = False) -> List:
         """
-        compare_word 와 badwords를 비교합니다
+        compare_word와 badwords를 비교하여 욕설인 부분과 그 퍼센트를 리턴합니다
+
+        :param badwords: 토큰화된 욕설 데이터입니다. self.BwT 또는 self.NewBwT를 입력하세요
+        :param compare_word: 토큰화된 문자열 데이터입니다. self.WTD[0] 또는 self.WTD[1]이 입력됩니다.
+        :param cut_line: 확률이 몇 이상이여야 욕설로 인식할지의 기준입니다. (0에서 1사이)
+        :param New: 초성 검사 모드로 할지 여부입니다.
+        :return: 욕설검사 결과를 리턴합니다.
         """
 
         result = []
